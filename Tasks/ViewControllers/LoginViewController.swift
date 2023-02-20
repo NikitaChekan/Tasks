b3241ce2a7aa32b2030eed24c621ae0e5e9c8cbd
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-02-21 00:03:05 +0300

    project preparation completed

--- Tasks/ViewControllers/LoginViewController.swift
+++ Tasks/ViewControllers/LoginViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var warningLabel: UILabel!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func ragisterTapped(_ sender: Any) {
+    }
+    
 }
 
--- Tasks/ViewControllers/TasksViewController.swift
+++ Tasks/ViewControllers/TasksViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var TasksTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 7
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
